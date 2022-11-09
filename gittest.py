61291ce327e98fb2241cad11b60104e369e106fb
shilpavasudev <shnrbkd@gmail.com>
2022-11-09 14:58:54 -0500

    added class for old file

--- gittest.py
+++ gittest.py
@@ -1,1 +1,13 @@
 print("hello world")
+class new():
+    def __init__(self):
+        pass
+    def sum(self,a,b):
+        self.a=a
+        self.b=b
+        return self.a+self.b
+
+s1=new()
+
+print(s1.sum(2,3))
+#print(new.sum(1,2))
